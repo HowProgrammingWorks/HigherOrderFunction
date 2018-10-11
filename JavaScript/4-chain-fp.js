68b22bfbab2aee32da8403bdd8539b8bc0ff9797
tshemsedinov <timur.shemsedinov@gmail.com>
2018-10-11 23:17:32 +0300

    Refactor chain example and reorder

--- JavaScript/4-chain-fp.js
+++
@@ -1,6 +0,0 @@
-'use strict';
-
-const fn = a => b => c => a + b + c;
-
-const res = fn(1)(2)(3);
-console.log('a + b + c = ' + res);
--- JavaScript/4-chain.js
+++ JavaScript/4-chain.js
@@ -8,8 +8,13 @@ function fn(a) {
   };
 }
 
+// const fn = a => b => c => a + b + c;
+
 const f1 = fn(1);
 const f2 = f1(2);
-const res = f2(3);
+const res1 = f2(3);
+const res2 = fn(1)(2)(3);
 
-console.log('a + b + c = ' + res);
+if (res1 === res2) {
+  console.log('a + b + c =', res1);
+}
