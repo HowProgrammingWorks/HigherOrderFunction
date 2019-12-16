c2e962994d0f9e857bb9cd56fc25d30f8a8251da
Destaby <53400745+Destaby@users.noreply.github.com>
2019-12-16 04:09:54 +0200

    Update 3-wrapper.js

--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,13 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...arr) => {
+  for (let i = 1; i < types.length - 1; i++) {
+    if (typeof fn(...arr) !== types[i].name.toLowerCase()) {
+      throw new TypeError('Types are different');
+    }
+  }
+  return fn(...arr);
+};
 
 module.exports = { contract };
+
