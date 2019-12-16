d1fb70ef5123cdc668dacc681c84ef7645637c03
Daniil Denisyuk <ordekonec@gmail.com>
2019-12-16 16:05:20 +0200

    work

--- Exercises/1-callback.js
+++ Exercises/1-callback.js
@@ -1,5 +1,16 @@
 'use strict';
 
-const iterate = (obj, callback) => null;
+const iterate = (object, callback) => {
+  for (let key in object){
+    if(object.hasOwnProperty(key))
+      callback(key, object[key]);
+  }
+//  for (let [key, value] of Object.entries(object) ) {
+//    callback(key, value);
+//  }
+//  Object.entries(object).forEach(([key, value])=>{
+//    callback(key, value);
+//})
+}
 
 module.exports = { iterate };
--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,17 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...args) => {
+  for(let index in args){
+    const arg = args[index];
+    if(arg !== types[index](arg)) throw new TypeError(`type of argument '${value}': '${typeof value}' does not match '${types[index]}'!`);
+  }
+//args.forEach((value, index) => {
+//    if(value !== types[index](value)) throw new TypeError(`type of argument '${value}': '${typeof value}' does not match '${types[index]}'!`);
+//  })
+  const res = fn(...args);
+  if(res !== types[types.length - 1](res))
+    throw new TypeError(`type of result '${res}': '${typeof res}' does not match '${types[types.length-1]}'!`);
+  return res;
+}
 
 module.exports = { contract };
