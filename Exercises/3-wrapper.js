48311cbf1d47fa46cb345425d374642320cc5ed7
ser <ser@localhost.localdomain>
2019-12-16 21:36:23 +0200

    done 1-3

--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,23 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...args) => {
+  for (const i in args) {
+    const Type = types[i];
+    const temp = new Type();
+    const type = typeof temp.valueOf();
+    const arg = args[i];
+    if (typeof arg !== type) {
+      throw new TypeError(`Неверный тип аргумента ${arg}`);
+    }
+  }
+  const res = fn(...args);
+  const TypeRes = types[types.length - 1];
+  const tempRes = new TypeRes();
+  const typeRes = typeof tempRes.valueOf();
+  if (typeof res !== typeRes) {
+    throw new TypeError(`Неверный тип результата ${res}`);
+  }
+  return res;
+};
 
 module.exports = { contract };
