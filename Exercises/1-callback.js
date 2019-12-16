e37b20df567da05acbe4d1ce6e66bd7ce8ef1585
Alexandr Shevqnov <shevqnov@gmail.com>
2019-12-16 14:45:26 +0500

    fix(excersises): fix eslint hints

--- Exercises/1-callback.js
+++ Exercises/1-callback.js
@@ -1,9 +1,9 @@
 'use strict';
 
 const iterate = (obj, cb) => {
-    Object.keys(obj).forEach(key => {
-        cb(key, obj[key], obj)
-    })
+  Object.keys(obj).forEach(key => {
+    cb(key, obj[key], obj);
+  });
 };
 
 module.exports = { iterate };
--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,23 +1,25 @@
 'use strict';
 
 const contract = (cb, ...types) => (...args) => {
-        args.forEach((arg, idx) => {
-            const declaredType = typeof types[idx]();
-            const actualType = typeof arg;
+  args.forEach((arg, idx) => {
+    const declaredType = typeof types[idx]();
+    const actualType = typeof arg;
 
-            if (declaredType !== actualType) {
-                throw TypeError(`type '${actualType}' do not match with type '${declaredType}'`);
-            }
-        });
-        const result = cb(...args);
-        const resultType = typeof result;
-        const declaredResultType = typeof types[types.length - 1]();
+    if (declaredType !== actualType) {
+      throw TypeError(`type '${actualType}' 
+            do not match with type '${declaredType}'`);
+    }
+  });
+  const result = cb(...args);
+  const resultType = typeof result;
+  const declaredResultType = typeof types[types.length - 1]();
 
-        if (resultType !== declaredResultType) {
-            throw TypeError(`type '${resultType}' do not match with type '${declaredResultType}'`);
-        }
+  if (resultType !== declaredResultType) {
+    throw TypeError(`type '${resultType}' 
+          do not match with type '${declaredResultType}'`);
+  }
 
-        return result;
+  return result;
 };
 
 module.exports = { contract };
