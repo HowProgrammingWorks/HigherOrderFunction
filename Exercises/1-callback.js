793a67e8d0b4537222f8c197866bed800a74d1ab
alexandr.pavletsov <alexandr.pavletsov@rcntec.com>
2019-12-28 23:07:05 +0500

    forEach everywere...

--- Exercises/1-callback.js
+++ Exercises/1-callback.js
@@ -1,10 +1,8 @@
 'use strict';
 
 const iterate = (obj, callback) => {
-  const keys = Object.keys(obj);
-  for (const key of keys) {
-    callback(key, obj[key], obj);
-  }
+  //i'm here because of limit min, but may be it's his wrong solution...
+  Object.keys(obj).forEach(key => callback(key, obj[key], obj));
 };
 
 module.exports = { iterate };
--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -4,9 +4,9 @@ const contract = (fn, ...types) => {
   const nFn = (...args) => {
     const resTyp = typeof(types.pop())();
     const argTyp = types.map(x => typeof x());
-    for (let i = 0; i < args.length; i++) {
+    Object.keys(args).forEach(i => {
       if (typeof args[i] !== argTyp[i]) throw new Error('Wrong args type');
-    }
+    });
     const res = fn(...args);
     if (typeof res !== resTyp) throw new Error('Wrong res type');
     return res;
