0e5280ad3be7171a3381ac5d4ffa8d82d27854f3
Grevcev.AS <grevcev.as@dns-shop.ru>
2021-11-01 07:41:30 +1000

    lab HigherOrderFunction

--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,24 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...args) => {
+  args.forEach((arg, index) => {
+    const typeName = types[index].name.toLowerCase();
+    const argType = typeof arg;
+    if (typeName !== argType) {
+      throw new TypeError(`Argument type expected:
+       ${typeName}, got: ${argType}`);
+    }
+  });
+  const result = fn(...args);
+  const expectedResultType = types[types.length - 1].name.toLowerCase();
+  const resultType = typeof result;
+
+  if (expectedResultType !== resultType) {
+    throw new TypeError(`Result type expected:
+     ${expectedResultType}, got: ${resultType}`);
+  }
+
+  return result;
+};
 
 module.exports = { contract };
