bf46a6364d48780f10ce2515d52800914844b958
vladimirvikulin <v.vikulin.im11@kpi.ua>
2021-12-10 15:48:02 +0200

    HigherOrderFunction Lab 8

--- Exercises/2-closure.js
+++ Exercises/2-closure.js
@@ -1,5 +1,12 @@
 'use strict';
 
-const store = x => null;
+// const store = value => {
+//   const storage = [value];
+//   return () => storage.shift();
+// };
+const store = value => () => value;
 
+const read = store(5);
+const value = read();
+console.log(value);
 module.exports = { store };
