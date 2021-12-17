39d4288c76daf0dd2aea4c8e48d8c365ca1648ee
Nailgy <ilyha.nali@gmail.com>
2021-12-17 03:30:36 +0200

    Tasks for HigherOrderFunction

--- Exercises/1-callback.js
+++ Exercises/1-callback.js
@@ -1,5 +1,10 @@
 'use strict';
 
-const iterate = (obj, callback) => null;
+const iterate = (obj, callback) => {
+  for (const field in obj) {
+    const value = obj[field];
+    callback(field, value, obj);
+  }
+};
 
 module.exports = { iterate };
--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,21 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...args) => {
+  for (let i = 0; i < types.length - 1; i++) {
+    const arg = args[i];
+    const type = types[i];
+    const name = type.name.toLowerCase();
+    if (typeof(arg) !== name) {
+      throw new TypeError(`Argument expected to be ${name}`);
+    }
+  }
+  const res = fn(...args);
+  const typeOfRes = types[types.length - 1];
+  const name = typeOfRes.name.toLowerCase();
+  if (typeof(res) !== name) {
+    throw new TypeError(`Result expected to be ${name}`);
+  }
+  return res;
+};
 
 module.exports = { contract };
