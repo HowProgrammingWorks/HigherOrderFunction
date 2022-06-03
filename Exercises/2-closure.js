b5668faf395ba14369db9ebdeb30b806c1bc1d85
orbis <infectores@gmail.com>
2022-06-03 23:09:28 +0300

    homework

--- Exercises/2-closure.js
+++ Exercises/2-closure.js
@@ -1,5 +1,5 @@
 'use strict';
 
-const store = x => null;
+const store = x => () => x;
 
 module.exports = { store };
--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,22 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract = (fn, ...types) => (...args) => {
+
+  for (let i = 0; i < types.length - 1; i++) {
+    const arg = args[i];
+    const curType = types[i].name.toLowerCase();
+
+    if (typeof arg !== curType) {
+      throw TypeError(`argument ${args[i]} is not a ${types[i].name}`);
+    }
+  }
+
+  const result = fn(...args);
+  const resultType = types[types.length - 1].name.toLowerCase();
+  if (typeof result !== resultType) {
+    throw TypeError(`result ${result} is not a ${resultType}`);
+  }
+  return result;
+};
 
 module.exports = { contract };
