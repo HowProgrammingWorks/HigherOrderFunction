6469351a3270a33127cefc3d671527b18631e6d8
Tima Gixe <timagixe@protonmail.com>
2023-02-15 17:22:45 +0200

    Solves 3-wrapper

--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,20 @@
 'use strict';
 
-const contract = (fn, ...types) => null;
+const contract =
+    (fn, ...types) =>
+    (...args) => {
+        const argTypes = types.slice(0, -1).map((type) => type.name.toLowerCase());
+        const returnType = types.at(-1).name.toLowerCase();
+        const result = fn(...args);
+        if (returnType !== typeof result) {
+            throw new TypeError(`Expected ${returnType}`);
+        }
+        for (const [index, arg] of args.entries()) {
+            if (typeof arg !== argTypes[index]) {
+                throw new TypeError(`Expected ${argTypes[index]}`);
+            }
+        }
+        return fn(...args);
+    };
 
 module.exports = { contract };
