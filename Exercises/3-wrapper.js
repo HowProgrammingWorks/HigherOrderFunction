81aaaf7cb51b74f850b917c7a89390ff64a39860
Jhon Mosk <jhon-mosk@yandex.ru>
2023-07-25 12:20:10 +0300

    All tasks completed

--- Exercises/3-wrapper.js
+++ Exercises/3-wrapper.js
@@ -1,5 +1,23 @@
 'use strict';
+// prettier-ignore
+const contract =
+  (fn, ...types) =>
+    (...args) => {
+      for (let i = 0; i < args; i++) {
+        const type = types[i];
+        const arg = args[i];
+        if (typeof arg !== type.name.toLowerCase()) {
+          throw new TypeError('Types do not match');
+        }
+      }
 
-const contract = (fn, ...types) => null;
+      const result = fn(...args);
+      const type = types.at(-1);
+      if (typeof result !== type.name.toLowerCase()) {
+        throw new TypeError('Types do not match');
+      }
+
+      return result;
+    };
 
 module.exports = { contract };
